b46801af668d6ff2240ff7a3ea110f04d28c39e7
KCCoderGirl <dontlaughatme@yahoo.com>
2022-05-04 11:33:02 -0500

    Thymeleaf through 13.5 complete

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -21,22 +21,31 @@ public class HelloController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String helloWithQueryParam(@RequestParam String name, Model model) {
+        model.addAttribute("greeting","Hello, " + name + "!");
+        return "hello";
     }
 
    // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting","Hello, " + name + "!");
+        return "hello";
     }
 
-    // /hello/form
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Jamie");
+        names.add("Megan");
+        names.add("Judy");
+        model.addAttribute("names",names);
+        return "hello-list";
     }
 
 }
